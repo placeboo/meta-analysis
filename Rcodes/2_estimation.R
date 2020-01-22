ed55387ec50cff1043a33dbe2f9a4e702edf2206
jiaqi <jiaqiyin1014@gmail.com>
2020-01-21 17:29:49 -0800

    estimation function for meta data

---
+++ Rcodes/2_estimation.R
@@ -0,0 +1,19 @@
+rm(list = ls())
+
+# --------- library and functions -------------
+source("Rcodes/0_library.R")
+source("Rcodes/0a_helper.R")
+
+# --------- load data ------------------------
+load("data/meta-data-generation-seed1.RData")
+
+# -------- estimation, corhort only ----------
+test = max.likelihood.v2(y = cohort.dat$y, 
+                              z = as.factor(cohort.dat$z), 
+                              va = cbind(cohort.dat$v1, cohort.dat$v2),
+                              vb = cbind(cohort.dat$v1, cohort.dat$v2),
+                              alpha.start = c(0,0),
+                              beta.start = c(0,0))
+
+
+# -------- estimation, both ----------
