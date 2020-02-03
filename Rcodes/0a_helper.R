b91daaf742da1dab03454aa932d027ad70c47e38
jiaqi <jiaqiyin1014@gmail.com>
2020-02-02 18:39:38 -0800

    update case control method

--- Rcodes/0a_helper.R
+++ Rcodes/0a_helper.R
@@ -1,103 +1,110 @@
 # ----- monotonic treatment estimator ------------
-max.likelihood.casecrt = function(y, z, va, vb, ps.mat,
-                                  alpha.start, beta.start,
-                                  max.step=10^4, thres=10^-6) {
-        if (!is.numeric(z)) stop("'z' should be numerical!")
+max.likelihood.casecrt = function(y, z, va, vb,
+                                   alpha.start, beta.start, eta.start,
+                                   max.step=10^4, thres=10^-6) {
+    if (!is.numeric(z)) stop("'z' should be numerical!")
+    
+    pa = length(alpha.start)
+    pb = length(beta.start)
+    pc = length(eta.start)
+    
+    ny = length(y)
+    
+    z.uniq = sort(unique(z))
+    nz = length(z.uniq)
+    
+    neg.log.value = function(alpha, beta, eta){
         
-        pa = length(alpha.start)
-        pb = length(beta.start)
-        ny = length(y)
+        ps = exp(va %*% eta) / (1 + exp(va %*% eta))
+        ps.mat = cbind(1-ps, ps)
         
-        z.uniq = sort(unique(z))
-        nz = length(z.uniq)
+        Pzmin_Pzmax = t(mapply(getProbScalarRR, va %*% alpha, vb %*% beta))
+        P.mat = matrix(0, ncol = nz, nrow = ny)
+        P.mat[, c(1, nz)] = Pzmin_Pzmax
         
-        neg.log.value = function(alpha, beta){
-                
-                Pzmin_Pzmax = t(mapply(getProbScalarRR, va %*% alpha, vb %*% beta))
-                P.mat = matrix(0, ncol = nz, nrow = ny)
-                P.mat[, c(1, nz)] = Pzmin_Pzmax
-                
-                P.mat[, -c(1, nz)] = Pzmin_Pzmax[,1] * exp(va %*% alpha %*% t(z.uniq)[-c(1,nz)])
-                
-                n_0.vec = apply(P.mat, 2, function(x) sum(x==0))
-                
-                if(identical(all.equal(n_0.vec, rep(0, nz)), TRUE)){
-                        value = 0
-                        
-                        for(i in 1: length(z.uniq)){
-                                for (j in c(0,1)) {
-                                        idx = (z == z.uniq[i]) & (y == j)
-                                        ps = ps.mat[idx, ]
-                                        py = j * P.mat[idx, ] + (1 - j) * (1 - P.mat[idx, ])
-                                        value = value - sum(log(py[, i]) + log(ps[,i]) - log(apply(ps * py, 1, sum)))
-                                }
-       
-                        }
-                        return(value)
-                }
-                else{
-                        return(10000)
-                }
+        P.mat[, -c(1, nz)] = Pzmin_Pzmax[,1] * exp(va %*% alpha %*% t(z.uniq)[-c(1,nz)])
+        
+        n_0.vec = apply(P.mat, 2, function(x) sum(x==0))
+        
+        if(identical(all.equal(n_0.vec, rep(0, nz)), TRUE)){
+            value = 0
+            py.mat = y * P.mat + (1 - y)  * (1 - P.mat)
+            
+            for (i in 1: length(z.uniq)){
+                value = value - sum(log(py.mat[z == z.uniq[i],i] * ps.mat[z == z.uniq[i],i]) - log(apply(py.mat[z == z.uniq[i], ] * ps.mat[z == z.uniq[i], ], 1, sum)))
+            }
+            return(value)
         }
-        neg.log.likelihood = function(pars){
-                alpha = pars[1:pa]
-                beta = pars[(pa + 1):(pa + pb)]
-                
-                return(neg.log.value(alpha, beta))
+        else{
+            return(10000)
         }
+    }
+    
+    
+    neg.log.likelihood = function(pars){
+        alpha = pars[1:pa]
+        beta = pars[(pa + 1):(pa + pb)]
+        eta = pars[(pa + pb + 1) : (pa + pb + pc)]
         
-        neg.log.likelihood.alpha = function(alpha){
-                return(neg.log.value(alpha, beta))
-        }
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    neg.log.likelihood.alpha = function(alpha){
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    neg.log.likelihood.eta = function(eta){
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    neg.log.likelihood.beta = function(beta){
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    Diff = function(x, y) sum((x - y)^2)/sum(x^2 + thres)
+    alpha = alpha.start
+    beta = beta.start
+    eta = eta.start
+    diff = thres + 1
+    step = 0
+    
+    
+    while(diff > thres & step < max.step){
+        step = step + 1
+        opt1 = stats::optim(alpha, neg.log.likelihood.alpha,
+                            control = list(maxit = max.step))
+        diff1 = Diff(opt1$par, alpha)
+        alpha = opt1$par
         
-        neg.log.likelihood.beta = function(beta){
-                
-                return(neg.log.value(alpha, beta))
-        }
+        opt2 = stats::optim(beta, neg.log.likelihood.beta, control = list(maxit = max.step))
         
-        Diff = function(x, y) sum((x - y)^2)/sum(x^2 + thres)
-        alpha = alpha.start
-        beta = beta.start
-        diff = thres + 1
-        step = 0
+        diff2 = Diff(opt2$par, beta)
+        beta = opt2$par
         
+        opt3 = stats::optim(eta, neg.log.likelihood.eta, control = list(maxit = max.step))
+        diff3 = Diff(opt3$par, eta)
+        eta = opt3$par
         
-        while(diff > thres & step < max.step){
-                step = step + 1
-                opt1 = stats::optim(alpha, neg.log.likelihood.alpha,
-                                    control = list(maxit = max.step))
-                diff1 = Diff(opt1$par, alpha)
-                alpha = opt1$par
-                
-                opt2 = stats::optim(beta, neg.log.likelihood.beta, control = list(maxit = max.step))
-                
-                diff2 = Diff(opt2$par, beta)
-                beta = opt2$par
-                
-                diff = max(diff1, diff2)
-        }
-        # have hessian matrix
-        hessian.mat = try(optimHess(c(alpha,beta), neg.log.likelihood), silent = TRUE)
-        
-        if ("try-error" %in% class(hessian.mat)) {
-                return()
-                alpha.cov = matrix(NA, pa+pb, pa+pb)
-        }
-        
-        cov = solve(hessian.mat)
-        point.est = c(alpha, beta)
-        
-        likelihood = exp(-neg.log.likelihood(point.est))
-        opt = orgEst(point.est = point.est,
-                     cov = cov,
-                     type = "monotone",
-                     name = c(paste("alpha", 1:ncol(va), sep = ""), paste("beta", 1:ncol(vb), sep = "")),
-                     va=va,
-                     vb=vb,
-                     coverged = step < max.step,
-                     likelihood = likelihood)
-        
-        return(opt)
+        diff = max(diff1, diff2, diff3)
+    }
+    # have hessian matrix
+    hessian.mat = try(optimHess(c(alpha,beta,eta), neg.log.likelihood), silent = TRUE)
+    
+    if ("try-error" %in% class(hessian.mat)) {
+        cov = matrix(NA, pa+pb+pc, pa+pb+pc)
+    } else cov = solve(hessian.mat)
+    
+    point.est = c(alpha, beta, eta)
+    
+    opt = orgEst(point.est = point.est,
+                 cov = cov,
+                 type = "monotone",
+                 name = c(paste("alpha", 1:ncol(va), sep = ""), paste("beta", 1:ncol(vb), sep = ""), paste("eta", 1:ncol(va), sep = "")),
+                 va=va,
+                 vb=vb,
+                 coverged = step < max.step)
+    
+    return(opt)
 }
 
 max.likelihood.all= function(case.control = list(y, z, va, vb),
