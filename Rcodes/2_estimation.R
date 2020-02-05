2558f5fcb2706ea9b1ec203275dc291031a7ea1c
jiaqi <jiaqiyin1014@gmail.com>
2020-02-04 18:15:13 -0800

    ps re-write

--- Rcodes/2_estimation.R
+++ Rcodes/2_estimation.R
@@ -6,8 +6,8 @@ source("Rcodes/0_library.R")
 source("Rcodes/0a_helper.R")
 
 # --------- load data ------------------------
-n1 = 1000
-n2 = 500
+n1 = 500
+n2 = 200
 seed = 2
 load( paste("data/crt", n1, "_casecrt", n2, "_seed", seed, ".RData", sep = ""))
 
@@ -23,36 +23,19 @@ test1 = brm(y = cohort.dat$y,
 
 # -------- estimation, case control only ----------
 # propensity score model
-ps.model = glm(casecrt.dat$z ~ casecrt.dat$v2, family = binomial(link='logit'))
-
+ps.model = glm(z ~ v2, data = cohort.dat, family = binomial(link='logit'))
+ps = predict(ps.model, type = "response", newdata = casecrt.dat)
+ps.mat = cbind(1-ps, ps)
 
 test2 = max.likelihood.casecrt(y = casecrt.dat$y, 
                                z = casecrt.dat$z, 
-                               va = cbind(casecrt.dat$v1, casecrt.dat$v2),
-                               vb = cbind(casecrt.dat$v1, casecrt.dat$v2),
-                               alpha.start = c(0,0),
-                               beta.start = c(0,0),
-                               eta.start = c(0,0))
-
-test3 = max.likelihood.casecrt(y = casecrt.dat$y, 
-                               z = casecrt.dat$z, 
-                               va = cbind(casecrt.dat$v1, casecrt.dat$v2),
-                               vb = cbind(casecrt.dat$v1, casecrt.dat$v2),
-                               alpha.start = c(0,0),
-                               beta.start = c(0,0),
-                               eta.start = ps.model$coefficients)
-# -------- estimation, both ----------
-ps.model1 = glm(c(cohort.dat$z, casecrt.dat$z) ~ c(cohort.dat$v2, casecrt.dat$v2), family = binomial(link='logit'))
-
-
-test4 = max.likelihood.casecrt(y = casecrt.dat$y, 
-                               z = casecrt.dat$z, 
+                               ps.mat = ps.mat,
                                va = cbind(casecrt.dat$v1, casecrt.dat$v2),
                                vb = cbind(casecrt.dat$v1, casecrt.dat$v2),
                                alpha.start = test1$point.est[c(1,2)],
-                               beta.start = test1$point.est[c(3,4)],
-                               eta.start = ps.model1$coefficients)
+                               beta.start = test1$point.est[c(3,4)])
 
+# -------- estimation, both ----------
 test.all = max.likelihood.all(case.control = list(y = casecrt.dat$y, 
                                                   z = casecrt.dat$z, 
                                                   va = cbind(casecrt.dat$v1, casecrt.dat$v2),
@@ -61,16 +44,13 @@ test.all = max.likelihood.all(case.control = list(y = casecrt.dat$y,
                                             z = cohort.dat$z, 
                                             va = cbind(cohort.dat$v1, cohort.dat$v2),
                                             vb = cbind(cohort.dat$v1, cohort.dat$v2)),
-                              alpha.start = c(0,0),
-                              beta.start = c(0,0),
-                              eta.start = ps.model1$coefficients)
+                              alpha.start = test1$point.est[c(1,2)],
+                              beta.start = test1$point.est[c(3,4)],
+                              eta.start = ps.model$coefficients)
                               
-
-
 
 test1$coefficients
 test2$coefficients
-test3$coefficients
-test4$coefficients
 test.all$coefficients
+
 
