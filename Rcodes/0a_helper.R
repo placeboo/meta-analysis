1d9e57f0e07e5f966c073fb78387b71a5a4fd6db
jiaqi <jiaqiyin1014@gmail.com>
2020-03-01 00:27:24 -0800

    revise case-control function

--- Rcodes/0a_helper.R
+++ Rcodes/0a_helper.R
@@ -1,5 +1,4 @@
-# ----- monotonic treatment estimator ------------
-max.likelihood.casecrt = function(y, z, va, vb, ps.mat,
+max.likelihood.casectrl = function(y, z, va, vb, ps.mat, 
                                   alpha.start, beta.start, 
                                   max.step=10^4, thres=10^-6) {
     if (!is.numeric(z)) stop("'z' should be numerical!")
@@ -89,6 +88,116 @@ max.likelihood.casecrt = function(y, z, va, vb, ps.mat,
                  name = c(paste("alpha", 1:ncol(va), sep = ""), paste("beta", 1:ncol(vb), sep = "")),
                  va=va,
                  vb=vb,
+                 coverged = step < max.step)
+    
+    return(opt)
+}
+
+max.likelihood.casectrl2 = function(y, z, va, vb,
+                                   alpha.start, beta.start, eta.start,
+                                   max.step=10^4, thres=10^-6) {
+    
+    pa = length(alpha.start)
+    pb = length(beta.start)
+    pc = length(eta.start)
+    
+    
+    ny = length(y)
+    
+    z.uniq = sort(unique(z))
+    nz = length(z.uniq)
+    
+    eta = eta.start #!!! Trick
+    
+    neg.log.value = function(alpha, beta, eta){
+        casecrt.ps =  exp(va %*% eta) / (1 + exp(va %*% eta))
+        ps.mat = cbind(1-casecrt.ps, casecrt.ps)
+        Pzmin_Pzmax = t(mapply(getProbScalarRR, va %*% alpha, vb %*% beta))
+        P.mat = matrix(0, ncol = nz, nrow = ny)
+        P.mat[, c(1, nz)] = Pzmin_Pzmax
+        
+        P.mat[, -c(1, nz)] = Pzmin_Pzmax[,1] * exp(va %*% alpha %*% t(z.uniq)[-c(1,nz)])
+        
+        n_0.vec = apply(P.mat, 2, function(x) sum(x==0))
+        
+        if(identical(all.equal(n_0.vec, rep(0, nz)), TRUE)){
+            value = 0
+            py.mat = y * P.mat + (1 - y)  * (1 - P.mat)
+            
+            for (i in 1: length(z.uniq)){
+                value = value - sum(log(py.mat[z == z.uniq[i],i] * ps.mat[z == z.uniq[i],i]) - log(apply(py.mat[z == z.uniq[i], ] * ps.mat[z == z.uniq[i], ], 1, sum)))
+            }
+            return(value)
+        }
+        else{
+            return(10000)
+        }
+    }
+    
+    
+    neg.log.likelihood = function(pars){
+        alpha = pars[1:pa]
+        beta = pars[(pa + 1):(pa + pb)]
+        eta = pars[(pa + pb + 1) : (pa + pb + pc)]
+        
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    neg.log.likelihood.alpha = function(alpha){
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    # neg.log.likelihood.eta = function(eta){
+    #      return(neg.log.value(alpha, beta, eta))
+    # }
+    
+    neg.log.likelihood.beta = function(beta){
+        return(neg.log.value(alpha, beta, eta))
+    }
+    
+    Diff = function(x, y) sum((x - y)^2)/sum(x^2 + thres)
+    alpha = alpha.start
+    beta = beta.start
+    # eta = eta.start
+    diff = thres + 1
+    step = 0
+    
+    
+    while(diff > thres & step < max.step){
+        step = step + 1
+        opt1 = stats::optim(alpha, neg.log.likelihood.alpha,
+                            control = list(maxit = max.step))
+        diff1 = Diff(opt1$par, alpha)
+        alpha = opt1$par
+        
+        opt2 = stats::optim(beta, neg.log.likelihood.beta, control = list(maxit = max.step))
+        
+        diff2 = Diff(opt2$par, beta)
+        beta = opt2$par
+        
+        # opt3 = stats::optim(eta, neg.log.likelihood.eta, control = list(maxit = max.step))
+        # diff3 = Diff(opt3$par, eta)
+        # eta = opt3$par
+        
+        # diff = max(diff1, diff2, diff3)
+        diff = max(diff1, diff2)
+    }
+    # have hessian matrix
+    hessian.mat = try(optimHess(c(alpha,beta,eta.start), neg.log.likelihood), silent = TRUE)
+    
+    
+    if ("try-error" %in% class(hessian.mat)) {
+        cov = matrix(NA, pa+pb+pc, pa+pb+pc)
+    } else cov = solve(hessian.mat)
+    
+    point.est = c(alpha, beta, eta.start)
+    
+    opt = orgEst(point.est = point.est,
+                 cov = cov,
+                 type = "monotone",
+                 name = c(paste("alpha", 1:pa, sep = ""), paste("beta", 1:pb, sep = ""), paste("eta", 1:pc, "")),
+                 va=va,
+                 vb=va,
                  coverged = step < max.step)
     
     return(opt)
